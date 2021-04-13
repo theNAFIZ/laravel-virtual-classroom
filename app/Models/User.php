6c2b98db8b822904731a2a302a6511e33c4365e5
theNAFIZ <nrpustcse@gmail.com>
2021-04-14 03:45:43 +0600

    User model methods implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,38 +6,53 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    protected $guarded = [];
+
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function isTeacher()
+    {
+        return $this->user_type == "Teacher" ? 1 : 0;
+    }
+
+
+    public function isStudent()
+    {
+        return $this->user_type == "Student" ? 1 : 0;
+    }
+
+
+    public function isAdmin()
+    {
+        return $this->user_type == "Admin" ? 1 : 0;
+    }
+
+
+    public function courses()
+    {
+        if ($this->isStudent())
+            return $this->belongsTo(Batch::class)->courses;
+        elseif ($this->isTeacher())
+            return $users = DB::table('courses')
+                ->where('teacher', $this)
+                ->get();
+        else
+            return Course::all();
+    }
 }
